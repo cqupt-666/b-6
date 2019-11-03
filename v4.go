e9f9360b243559fe57d9510f8194bde5ab15e18d
cqupt-666 <57167094+cqupt-666@users.noreply.github.com>
2019-11-03 13:50:01 +0800

    Add files via upload

--- v4.go
+++ v4.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"bytes"
+
 	"fmt"
-	"io"
+	"io/ioutil"
+
 	"os"
-	"strings"
 )
-type Reader interface {
 
-	Read(p []byte) (n int, err error)
-
-}
-type Writer interface {
-
-	Write(p []byte) (n int, err error)
-
-}
-
+	var f *os.File
 func main() {
 	f, err := os.Create("proverb.txt")
 	if err != nil {
@@ -25,72 +16,8 @@ func main() {
 		f.Close()
 		return
 	}
-	proverbs := []string{
-
-		"don't communicate by sharing memory share memory by communicating",
-	}
-
-	var writer bytes.Buffer
-
-
-	for _, p := range proverbs {
-
-		n, err := writer.Write([]byte(p))
-
-		if err != nil {
-
-			fmt.Println(err)
-
-			os.Exit(1)
-
-		}
-
-		if n != len(p) {
-
-			fmt.Println("failed to write data")
-
-			os.Exit(1)
-
-		}
-
-	}
-
-
-	fmt.Println(writer.String())
-
-	reader := strings.NewReader("don't communicate by sharing memory share memory by communicating")
-
-	p := make([]byte, 65)
-
-
-	for {
-
-		n, err := reader.Read(p)
-
-		if err != nil{
-
-			if err == io.EOF {
-
-				fmt.Println("EOF:", n)
-
-				break
-
-			}
-
-			fmt.Println(err)
-
-			os.Exit(1)
-
-		}
-
-		fmt.Println(n, string(p[:n]))
-
-	}
-
-
-
+	f.Write([]byte("don't communicate by sharing memory share memory by communicating"))
+	buf,_:=ioutil.ReadFile("proverb.txt")
+	fmt.Println(string(buf))
 
 }
-
-
-
